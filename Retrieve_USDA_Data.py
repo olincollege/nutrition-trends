da7ec7b3b89e5ad364cb338f92d611a826f5e9e0
Brandon Spiller <bspiller@olin.edu>
2025-03-13 23:30:00 -0400

    Figured out library 1 data

--- Retrieve_USDA_Data.py
+++ Retrieve_USDA_Data.py
@@ -1,6 +1,18 @@
 from usda.client import UsdaClient
 from SecureINformation import API_Key
-usdaclinet = UsdaClient(API_Key)
 
-for value in usdaclinet.list_nutrients(10, sort="r"):
-    print(value)
+client =  UsdaClient("OBKSK3PIBmtPIoIxZOUyuttIZIVk9v5vPiMsQBHb")
+
+foods = client.list_foods(5)
+
+for food in foods:
+    print(food.name)
+
+#usdaclinet = UsdaClient(API_Key)
+
+#for value in usdaclinet.list_nutrients(10, sort="r"):
+#    print(value)
+#
+
+
+##THIS DOES NOT WORK BTW
