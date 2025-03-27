f21288506a48c37637537c6b43e6d6812d78d27e
Brandon Spiller <bspiller@olin.edu>
2025-03-27 01:00:25 -0400

    updated data manipulation file with visualizations

--- datamanipulation.py
+++ datamanipulation.py
@@ -1,6 +1,6 @@
 import json
 
-with open("test.json", encoding="utf-8") as file:
+with open("test2.json", encoding="utf-8") as file:
     data_string = file.read()
     data = json.loads(data_string)
 
@@ -8,25 +8,25 @@ with open("test.json", encoding="utf-8") as file:
 
 # Top american foods according to google
 american_food_keys = [
-    "Pizza",
+    "Fast Food, Pizza",
     "Fried Chicken",
-    "Mac and Cheese",
-    "Hot Dogs",
-    "Sandwiches",
+    "Hamburger",
+    "Hot Dog",
+    "Grilled Cheese Sandwich",
     "Spaghetti with Meatballs",
-    "Tacos",
-    "Salads",
+    "Taco",
+    "Caesar Salad",
     "Pancakes",
 ]
 # preseting the intial food in the dictionary
 matched_food = {key: None for key in american_food_keys}
-print(matched_food)
+# print(matched_food)
 
 for food in data:
     description = food.get(
         "description", ""
     ).lower()  # defines description in USDA data and makes it all lowercase
-    print("Checking USDA description:", description)
+    # print("Checking USDA description:", description)
 
     for american_food in american_food_keys:  # start of the for loop
         if american_food.lower() in description:
@@ -43,11 +43,6 @@ for food in data:
 print(matched_food)
 
 
-# def esther_suffer(food):
-#     """this is what I hate"""
-#     matched_food = {key: None for key in american_food_keys}
-
-
 def get_food_details(food):
     """
     This function takes the food name/id/description and returns the amounts
@@ -60,79 +55,69 @@ def get_food_details(food):
         a new list that consists of the food as keys and the 4 nutrient
         amounts as the values
     """
-    nutrient_list = []
+    nutrient_order = {
+        "203": 0,  # Protein
+        "606": 1,  # Fatty Acids
+        "205": 2,  # Carbohydrates
+        "269": 3,  # Sugars
+        "291": 4,  # Fiber
+        "307": 5,  # Sodium
+        "255": 6,  # Water
+    }
+    nutrient_list = [0] * 7
 
     for nutrient in food.get("foodNutrients", []):
-        if nutrient.get("number") == "203":  # protein
-            nutrient_list.append(nutrient.get("amount"))
-        elif nutrient.get("number") == "606":  # fatty acids
-            nutrient_list.append(nutrient.get("amount"))
-        elif nutrient.get("number") == "205":  # carbohydrates
-            nutrient_list.append(nutrient.get("amount"))
-        elif nutrient.get("number") == "269":  # sugars
-            nutrient_list.append(nutrient.get("amount"))
+        nutrient_number = nutrient.get("number")
+        if nutrient_number in nutrient_order:
+            nutrient_list[nutrient_order[nutrient_number]] = nutrient.get(
+                "amount"
+            )
 
     return nutrient_list
 
 
-# def get_food_details(food):
-#     """
-#     This function takes the food name/id/description and returns the amounts
-#     of 4 certain nutrients (protein, fatty acids, Carbohydrates, and Sugars)
-#     in the form of a list ordered the same way it is above
-
-#     Args:
-#         food: a string that represents the food being searched
-#     Returns:
-#         a new list that consists of the food as keys and the 4 nutrient
-#         amounts as the values
-#     """
-
-#     # This part is definitely wrong but I have no idea what I am doing
-#     nutrient_list = []
-#     amount = food.get("amount", "")
-#     number = food.get("number", "")
-
-#     for american_food, value in matched_food():
-#         # protein
-#         if i.get(number) == "203":
-#             nutrient_list.append(i.get(amount))
-#         # fatty acids
-#         if i.get(number) == "606":
-#             nutrient_list.append(i.get(amount))
-#         # carbohydrates
-#         if i.get(number) == "205":
-#             nutrient_list.append(i.get(amount))
-#         # sugars
-#         if i.get(number) == "269":
-#             nutrient_list.append(i.get(amount))
-#         return nutrient_list
-
-# ##This part is definitely wrong but I have no idea what I am doing
-# nutrient_list = []
-# for i in data[food]:
-#     # protein
-#     if i.get("number") == "203":
-#         nutrient_list.append(i.get("amount"))
-#     # fatty acids
-#     if i.get("number") == "606":
-#         nutrient_list.append(i.get("amount"))
-#     # carbohydrates
-#     if i.get("number") == "205":
-#         nutrient_list.append(i.get("amount"))
-#     # sugars
-#     if i.get("number") == "269":
-#         nutrient_list.append(i.get("amount"))
-#     return nutrient_list
-
-
 food_dict = {}
 
+# for food_name, usda_item in matched_food.items():
+#     if usda_item:  # not None
+#         food_dict[food_name] = get_food_details(usda_item)
+#     else:
+#         food_dict[food_name] = None
+
 for food_name, usda_item in matched_food.items():
-    if usda_item:  # not None
+    if usda_item:  # Ensure the item is not None
         food_dict[food_name] = get_food_details(usda_item)
     else:
-        food_dict[food_name] = None
-
+        food_dict[food_name] = [0] * 7
 
 print(food_dict)
+
+# this code gives us the total nutrients of the top 10 most eaten foods
+total_nutrients = []
+list_len = [0, 1, 2, 3, 4, 5, 6]
+for i in list_len:
+    total_one_nutrient = sum(values[i] for values in food_dict.values())
+    total_nutrients.append(total_one_nutrient)
+print(total_nutrients)
+
+
+# order is proteins, fats, carbs, sugars, fibers, sodium, and water
+
+nrf_score_list = []
+for food, values in food_dict.items():
+    nrf_score = ((values[0] / 50) + (values[4] / 28) + (values[6] / 500)) - (
+        (values[1] / 20)
+        + (values[3] / 50)
+        + (values[5] / 2000)
+        + (values[2] / 275)
+    )
+    nrf_score_list.append(nrf_score)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.bar(american_food_keys, nrf_score_list)
+plt.title("NRF Score of the 10 most common foods in America")
+plt.xlabel("Foods")
+plt.ylabel("NRF Score")
+plt.show()
